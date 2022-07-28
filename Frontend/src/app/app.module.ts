04610860ee9198f68565101a2be05054889f703b
Ananth krishnan A <55017917+ananthanil@users.noreply.github.com>
2022-07-28 23:53:18 +0530

    frontend compents created

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { BookComponent } from './book/book.component';
+import { AddBookComponent } from './add-book/add-book.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomePageComponent,
+    SignupComponent,
+    LoginComponent,
+    BookComponent,
+    AddBookComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
