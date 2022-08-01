c666152ab047a98ac0ae2fddb7142f748d9eba41
Ananth krishnan A <55017917+ananthanil@users.noreply.github.com>
2022-08-01 17:44:18 +0530

    added backend folder and required files.

---
+++ Backend/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+var bodyparser = require('body-parser');
+var app = new express();
+app.use(cors());
+app.use(bodyparser.json())
+
+app.listen(3006,function(){
+    console.log('listening to port 3006');
+  });
