ceb8533c5d2d9c75b466041c93f571455e784dda
Ananth krishnan A <55017917+ananthanil@users.noreply.github.com>
2022-08-01 19:17:40 +0530

    added

--- Frontend/src/app/book/book.component.ts
+++ Frontend/src/app/book/book.component.ts
@@ -6,8 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book.component.css']
 })
 export class BookComponent implements OnInit {
+  title:String = "Product List";
+
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+
+  showImage: boolean = false;
 
   constructor() { }
+  toggleImage(): void{
+    this.showImage = !this.showImage;
+  }
 
   ngOnInit(): void {
   }
